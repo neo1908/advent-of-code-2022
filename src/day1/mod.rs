16040254fd6351dfb62b6492d14d64a6737ea5ab
Ben <0x620x64@protonmail.com>
2022-12-01 18:40:06 +0000

    Reformat with cargofmt

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -3,31 +3,25 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Elf
-{
+pub struct Elf {
     id: u32,
     calories: u32,
 }
 
 // Implement "reverse" ordering - largest first
-impl Ord for Elf
-{
-    fn cmp(&self, other: &Self) -> Ordering
-    {
+impl Ord for Elf {
+    fn cmp(&self, other: &Self) -> Ordering {
         other.calories.cmp(&self.calories)
     }
 }
 
-impl PartialOrd for Elf
-{
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering>
-    {
+impl PartialOrd for Elf {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(other.calories.cmp(&self.calories))
     }
 }
 
-pub fn compute_calories(path: &str) -> Result<Vec<Elf>, Box<dyn std::error::Error>>
-{
+pub fn compute_calories(path: &str) -> Result<Vec<Elf>, Box<dyn std::error::Error>> {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
 
@@ -36,79 +30,101 @@ pub fn compute_calories(path: &str) -> Result<Vec<Elf>, Box<dyn std::error::Erro
     let mut elf_id = 0;
     let mut elf_calories = 0;
 
-    for line in reader.lines()
-    {
+    for line in reader.lines() {
         let parsed_line = line?;
 
-        if parsed_line.is_empty()
-        {
-            computed_calories.push(Elf{id: elf_id, calories: elf_calories});
+        if parsed_line.is_empty() {
+            computed_calories.push(Elf {
+                id: elf_id,
+                calories: elf_calories,
+            });
             elf_id += 1;
             elf_calories = 0;
-        }
-        else
-        {
+        } else {
             elf_calories += parsed_line.parse::<u32>()?;
         }
     }
 
     // It'd be nice to do this in one go - detect the last line as part of the loop
-    computed_calories.push(Elf{id: elf_id, calories: elf_calories });
+    computed_calories.push(Elf {
+        id: elf_id,
+        calories: elf_calories,
+    });
 
     Ok(computed_calories)
 }
 
-pub fn find_elf_with_largest_calories(computed_calories: &[Elf]) -> Option<&Elf>
-{
-    computed_calories.iter().reduce(|a,b| if a.calories > b.calories {a} else { b })
+pub fn find_elf_with_largest_calories(computed_calories: &[Elf]) -> Option<&Elf> {
+    computed_calories
+        .iter()
+        .reduce(|a, b| if a.calories > b.calories { a } else { b })
 }
 
-pub fn compute_total_calories_of_slice(elves: &[Elf], slice_size: usize, order: bool) -> Option<u32>
-{
+pub fn compute_total_calories_of_slice(
+    elves: &[Elf],
+    slice_size: usize,
+    order: bool,
+) -> Option<u32> {
     let mut local_vec = elves.to_vec();
-    if order
-    {
+    if order {
         local_vec.sort();
     }
 
-    local_vec.as_slice()[0..slice_size].iter().map(|e| e.calories).reduce(|a,b| a + b )
+    local_vec.as_slice()[0..slice_size]
+        .iter()
+        .map(|e| e.calories)
+        .reduce(|a, b| a + b)
 }
 
 #[cfg(test)]
-mod tests
-{
+mod tests {
     use super::*;
 
     const INPUT: &str = "src/test_inputs/day1_example.txt";
 
     #[test]
-    fn test_example_input()
-    {
+    fn test_example_input() {
         let mut expected = vec![];
-        expected.push(Elf{id: 0, calories: 6000});
-        expected.push(Elf{id: 1, calories: 4000});
-        expected.push(Elf{id: 2, calories: 11000});
-        expected.push(Elf{id: 3, calories: 24000});
-        expected.push(Elf{id: 4, calories: 10000});
+        expected.push(Elf {
+            id: 0,
+            calories: 6000,
+        });
+        expected.push(Elf {
+            id: 1,
+            calories: 4000,
+        });
+        expected.push(Elf {
+            id: 2,
+            calories: 11000,
+        });
+        expected.push(Elf {
+            id: 3,
+            calories: 24000,
+        });
+        expected.push(Elf {
+            id: 4,
+            calories: 10000,
+        });
 
-        assert_eq!(expected, compute_calories( INPUT ).unwrap() );
+        assert_eq!(expected, compute_calories(INPUT).unwrap());
     }
 
     #[test]
-    fn test_find_largest_elf()
-    {
-        let expected = Elf{id: 3, calories: 24000};
-        let computed = compute_calories(INPUT ).unwrap();
-        let result = find_elf_with_largest_calories(&computed ).unwrap();
+    fn test_find_largest_elf() {
+        let expected = Elf {
+            id: 3,
+            calories: 24000,
+        };
+        let computed = compute_calories(INPUT).unwrap();
+        let result = find_elf_with_largest_calories(&computed).unwrap();
 
         assert_eq!(&expected, result);
     }
 
     #[test]
-    fn test_compute_total_calories()
-    {
+    fn test_compute_total_calories() {
         let expected = 45000;
-        let mut computed = compute_calories(INPUT ).unwrap();
+        let mut computed = compute_calories(INPUT).unwrap();
 
         let result = compute_total_calories_of_slice(&mut computed, 3, true).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod day1;
 
 fn main() {
-    let computed_calories = day1::compute_calories("inputs/day1.txt").expect("Error when computing calories");
+    let computed_calories =
+        day1::compute_calories("inputs/day1.txt").expect("Error when computing calories");
 
-    let largest_elf = day1::find_elf_with_largest_calories(&computed_calories).expect("Error when finding the big elf");
+    let largest_elf = day1::find_elf_with_largest_calories(&computed_calories)
+        .expect("Error when finding the big elf");
     println!("Largest Elf is {:?}", largest_elf);
 
-    let top_3_calorie_total = day1::compute_total_calories_of_slice(&computed_calories, 3, true).expect("Error when computing slice total");
+    let top_3_calorie_total = day1::compute_total_calories_of_slice(&computed_calories, 3, true)
+        .expect("Error when computing slice total");
     println!("Top 3 total {}", top_3_calorie_total);
 }
